443df1108d03652de29ed56bb189646eea60df80
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-17 00:06:41 -0700

    first commit

---
+++ first_app.js
@@ -0,0 +1,4 @@
+//console.log("My first_app in nodeJs");
+//write to a file
+const fs= require('fs');
+fs.writeFileSync('hello.txt', 'NodeJs is a Javascript runtime that adds functionality to javascript and allows javascript to run on a server');
